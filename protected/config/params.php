3247dc174ca2fe846a43f7b09c997f28b1482ebc
Mark Bryk <bryk@cooper.edu>
2013-01-10 18:53:39 -0500

    into a yii app

---
+++ protected/config/params.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+ // Some default parameters can be here (they may be overwritten in $frontendParamsLocal
+ 'db.username'=>'root',
+ 'db.password'=>'',
+ 'db.name'=>'shalhevet',
+ 'db.host'=>'localhost',
+/*
+ 'db.username'=>'markbr6_root',
+ 'db.password'=>'abcde',
+ 'db.name'=>'markbr6_solosports',
+ 'db.host'=>'localhost',    
+ */   
+);
